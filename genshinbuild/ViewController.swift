d76bec63e64a5196e0860fa361c595d1727e5851
Nolan <iamchunkerdunkers@gmail.com>
2023-02-28 09:54:14 -0700

    Initial Commit

---
+++ genshinbuild/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  genshinbuild
+//
+//  Created by Laake, Nolan - Student on 2/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ genshinbuildUITests/genshinbuildUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  genshinbuildUITestsLaunchTests.swift
+//  genshinbuildUITests
+//
+//  Created by Laake, Nolan - Student on 2/28/23.
+//
+
+import XCTest
+
+final class genshinbuildUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
